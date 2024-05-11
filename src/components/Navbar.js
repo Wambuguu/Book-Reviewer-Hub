59c4dda5637a0041159ec2e6a0691046a226660e
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-11 12:46:30 +0300

    Add a div for the navbar container & the logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src={logo} alt="logo" width={70} height={70} />
+        </div>
+      </div>
+    </div>
+  );
+};
