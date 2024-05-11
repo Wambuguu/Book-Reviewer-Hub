037ee216b118e65933cada60308015537ee1045f
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-11 13:02:58 +0300

    Add a home page & a background image

---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const Home = () => {
+    const backgroundImageUrl =
+      "https://img.freepik.com/free-photo/textbooks-lying-near-bookcase_23-2147845949.jpg?t=st=1714996178~exp=1714999778~hmac=25e4163d13a4b068fb76799e46801d33c05657737b12bf2949adb1cac3bb8a0e&w=900";
+}
+  
