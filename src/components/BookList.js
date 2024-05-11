e5f07818da7670200d538fdbc509abf5ab6cae35
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-11 13:41:48 +0300

    Add state variables

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,2 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
+const BookList = () => {
+    const [books, setBooks] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [selectedBook, setSelectedBook] = useState(null);
+}
