eab3277e0d3950825bd010bf5cec24e00992f249
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-11 14:47:23 +0300

    Add function to fetch reviews

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from "react";
 
 function ReviewForm() {
-    const [bookTitle, setBookTitle] = useState("");
-    const [bookAuthor, setBookAuthor] = useState("");
-    const [bookReview, setBookReview] = useState("");
-    const [bookRating, setBookRating] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
-    const [successMessage, setSuccessMessage] = useState("");
-    const [reviews, setReviews] = useState([]);
+  const [bookTitle, setBookTitle] = useState("");
+  const [bookAuthor, setBookAuthor] = useState("");
+  const [bookReview, setBookReview] = useState("");
+  const [bookRating, setBookRating] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [reviews, setReviews] = useState([]);
+
+  function fetchReviews() {
+    fetch("https://books-api-iy0g.onrender.com/reviews")
+      .then((response) => response.json())
+      .then((data) => {
+        setReviews(data);
+      })
+      .catch((error) => console.error("Error fetching reviews:", error));
+  }
 }
