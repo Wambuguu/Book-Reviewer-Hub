bddf83df0c966dd2cac5102a0eed13dac127103d
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-11 12:49:41 +0300

    Add home, books, about & add review links in the navbar together with styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,22 @@ const Navbar = () => {
         <div>
           <img src={logo} alt="logo" width={70} height={70} />
         </div>
+        <nav className="flex flex-row gap-4">
+          <ul className="flex flex-row items-center gap-4 ">
+            <li className="hover:bg-blue-700 text-white font-bold">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="hover:bg-blue-700 text-white font-bold">
+              <Link to="/books">Books</Link>
+            </li>
+            <li className="hover:bg-blue-700 text-white font-bold">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="hover:bg-blue-700 text-white font-bold">
+              <Link to="/add-review">Add Review</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
