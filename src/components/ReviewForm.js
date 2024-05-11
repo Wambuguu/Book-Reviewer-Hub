26413dacdabf300acd7390a2b2d3d75dc77ff71d
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-11 14:45:49 +0300

    Add state variables

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,1 +1,11 @@
 import React, { useState, useEffect } from "react";
+
+function ReviewForm() {
+    const [bookTitle, setBookTitle] = useState("");
+    const [bookAuthor, setBookAuthor] = useState("");
+    const [bookReview, setBookReview] = useState("");
+    const [bookRating, setBookRating] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
+    const [reviews, setReviews] = useState([]);
+}
