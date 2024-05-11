bae00995ceef466fc125bd94921965adc72cca1b
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-11 13:55:20 +0300

    Fetch book data using the provided API

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,7 +2,22 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const BookList = () => {
-    const [books, setBooks] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
-    const [selectedBook, setSelectedBook] = useState(null);
-}
+  const [books, setBooks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await axios.get(
+          "https://books-api-iy0g.onrender.com/books"
+        );
+        setBooks(response.data);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+      }
+    };
+
+    fetchBooks();
+  }, []);
+};
