293e325ada8c7b354d30797f1552100c185bd660
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-11 15:44:25 +0300

    Update styles

--- src/pages/AddReviewPage.js
+++ src/pages/AddReviewPage.js
@@ -4,7 +4,7 @@ import ReviewForm from '../components/ReviewForm';
 const AddReviewPage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold mb-4 text-center">Add Review Page</h1>
+      <h1 className="text-3xl font-bold mb-4 text-center py-7">Add Review Page</h1>
       <ReviewForm />
     </div>
   );
