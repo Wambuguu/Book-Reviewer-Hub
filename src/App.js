6672b9a5d063edab3d6e4488b7e19ab5b92adedf
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-11 15:25:29 +0300

    Add the home, booklist, about & addreview routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import BookList from "./components/BookList";
+import AddReviewPage from "./pages/AddReviewPage";
+import About from "./components/About";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {" "}
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/books" element={<BookList />} />
+          <Route exact path="/about" element={<About/>} />
+          <Route path="/add-review" element={<AddReviewPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
