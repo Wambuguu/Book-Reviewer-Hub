b67c2673372f8b61e1d739fa8552c27570343765
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-11 13:27:47 +0300

    Add a div for the navigation buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -68,6 +68,21 @@ const Home = () => {
             <p>{points[currentPointIndex].description}</p>
           </div>
         </div>
+
+        <div>
+          <button
+            className="bg-white text-gray-900 rounded-full px-4 py-2 mr-4 focus:outline-none"
+            onClick={handlePrevPoint}
+          >
+            Prev
+          </button>
+          <button
+            className="bg-white text-gray-900 rounded-full px-4 py-2 focus:outline-none"
+            onClick={handleNextPoint}
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
