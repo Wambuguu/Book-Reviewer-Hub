427a94f999e181cc01f724bb58a3457f3f5f809f
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-11 15:14:26 +0300

    Add a footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-center text-m font-bold mt-4 p-4">
+      <p>&copy; 2024 Book Review Platform. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
