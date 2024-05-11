86211d1569c1c75b1e4d972784671bb9d5646ac5
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-11 15:12:37 +0300

    Add an addreviewpage & link it to the reviewform component

---
+++ src/pages/AddReviewPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReviewForm from '../components/ReviewForm';
+
+const AddReviewPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4 text-center">Add Review Page</h1>
+      <ReviewForm />
+    </div>
+  );
+};
+
+export default AddReviewPage;
